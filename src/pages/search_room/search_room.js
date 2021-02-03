7091490ea0157cfd6db63698c7391cf45a8e5f57
bogdanSobolev <bogdan.sobolev192837@gmail.com>
2021-02-03 12:53:52 +0300

    готов список комнат для search_room

--- src/pages/search_room/search_room.js
+++ src/pages/search_room/search_room.js
@@ -2,3 +2,11 @@ require('!test-loader-two?extensions[]=js&extensions[]=scss&levels[]=src/compone
 
 import '../../layout/layout.scss';
 import './search_room.scss';
+
+import $ from 'jquery';
+
+$('.search-room__filter-btn').on('click', (e) => {
+    $(e.currentTarget).toggleClass('search-room__filter-btn_active');
+    $(e.currentTarget).siblings('.search-room__aside-form').toggleClass('search-room__aside-form_active');
+    console.log($(e.currentTarget).siblings('.search-room__aside-form'));
+});
