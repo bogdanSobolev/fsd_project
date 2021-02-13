5777537d361f0b8f89c2a4449c0e7e634bc9ecff
bogdanSobolev <bogdan.sobolev192837@gmail.com>
2021-02-13 18:51:21 +0300

    добавлен favicon

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,5 @@
 require('!bemdecl-to-fs-loader?extensions[]=js&extensions[]=scss&levels[]=src/components!html2bemdecl-loader!pug-html-loader!./index.pug');
 
+import '../../favicons/favicons';
 import '../../layout/layout.scss';
 import './index.scss';
